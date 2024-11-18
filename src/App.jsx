ba6ac130236a419b8a21e1ded58fcea0cf71ac63
Gaurav Kumar <gauravkumar@Gauravs-MacBook.local>
2024-11-18 15:59:38 +0530

    Added search features/files for CryptoTracker

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-// import { useState } from 'react'
+import { useState,useEffect } from 'react'
+import logo from './assets/bitc.png'; // Import your logo
+
+
 //  import { CurrencyContext } from './context/CurrencyContext';
 
 import Routing from './components/Routing/Routing'
 
 
 function App() {
- 
+      useEffect(() => {
+        document.title = 'Cryptocurrency'; // Set the tab name here
+      }, []); // This will run once when the component mounts
+
   // const [currency,setCurrency]=useState('usd');
 
   return (
     <>
+    
       {/* <CurrencyContext.Provider value={{currency,setCurrency}}>
         <Home/>
       </CurrencyContext.Provider> */}
